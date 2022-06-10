3bf11ad5c6bec1c19d9acdcbd77f65ae3bd8e349
dfvalencia <dfvalenc@bancolombia.com.co>
2022-06-10 12:33:48 -0500

    adding mapview annotatios

---
+++ VirtualTouristApp/View/UIViewController.swift
@@ -0,0 +1,45 @@
+//
+//  UIViewController.swift
+//  VirtualTouristApp
+//
+//  Created by Daniel Felipe Valencia Rodriguez on 10/06/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func buttonEnabled(_ enabled: Bool, button: UIButton) {
+        if enabled {
+            button.isEnabled = true
+            
+        } else {
+            
+            button.isEnabled = false
+        }
+    }
+    
+    func showAlert(message: String, title: String) {
+        let alertVC = UIAlertController (title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertVC, animated: true)
+}
+    
+    //open links
+    
+    func openLink(_ url: String) {
+        guard let url = URL(string: url), UIApplication.shared.canOpenURL(url)
+            else {
+                showAlert(message: "Cannot open link", title: "Invalid URL")
+                return
+        }
+}
+    
+    func showFailure(message: String) {
+        let alert = UIAlertController(title: "ALERT", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
--- VirtualTouristApp/ViewController.swift
+++ VirtualTouristApp/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var initialLatitude : Double = 4.624335
+    var initialLongitude : Double = -74.063644
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        showMapAnnotations()
+    }
+    
+    
+    func showMapAnnotations () {
+        var annotations = [MKPointAnnotation]()
+        let latitude = CLLocationDegrees(initialLatitude)
+        let longitude = CLLocationDegrees(initialLongitude)
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let annotation = MKPointAnnotation ()
+        annotation.coordinate = coordinate
+        annotation.title = "Colombia"
+        annotation.subtitle = "Bogotá"
+        annotations.append(annotation)
+        self.mapView.addAnnotations(annotations)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKMarkerAnnotationView
+        if pinView == nil {
+            pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier:reuseId)
+            pinView!.canShowCallout = true
+            pinView!.markerTintColor = .black
+            pinView?.rightCalloutAccessoryView = UIButton(type: .infoDark)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    
+   func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let url = URL(string: (view.annotation?.subtitle!)!)
+            if  !verifyUrl(urlString: view.annotation?.subtitle!) {
+                self.showFailure(message: "Invalid URL!")
+            } else {
+                UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    func verifyUrl(urlString: String?) -> Bool {
+        guard let urlString = urlString,
+              let url = URL(string: urlString) else {
+            return false
+        }
+
+        return UIApplication.shared.canOpenURL(url)
     }
 
 
+    
 }
 
